bdef1dd6fa23d3cc480ae5e5f5c8886baa528155
Connor Graham <thecg1997@gmail.com>
2015-07-30 23:38:48 +0100

    Added code to Github

---
+++ pe/nn/connor/directorydemacifier/DirectoryDemacifier.java
@@ -0,0 +1,49 @@
+package pe.nn.connor.directorydemacifier;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DirectoryDemacifier {
+	private static String applicationVersion = "V1.0.1";
+	
+	public static void main(String[] args) {
+		if(args.length == 0){
+			System.err.println("directorydemacifier PATH [--nosubfolders]");
+			System.exit(0);
+		}
+		
+		List<String> arguments = new ArrayList<String>();
+		
+		for(String argument : args){
+			arguments.add(argument);
+		}
+		
+		File path = new File(args[0]);
+		if(path.exists()){
+			if(path.isDirectory()){
+				doStuff(path, arguments);
+			}else{
+				System.err.println("PATH must be directory");
+			}
+			
+			System.out.println(String.format("Thanks for using DirectoryRemacifier V%s by Connor Graham (thecg.me)", applicationVersion));
+		}else{
+			System.err.println("PATH must exist");
+		}
+	}
+	
+	private static void doStuff(File directory, List<String> arguments){
+		for(File file : directory.listFiles()){
+			if(file.getName().startsWith("._") || file.getName().startsWith(".Spotlight") || file.getName().equals(".Trashes")){
+				System.out.println("Deleted " + file.getAbsolutePath());
+				file.delete();
+			}
+			if(file.isDirectory() && file.exists()){
+				if(!arguments.contains("--nosubfolders")){
+					doStuff(file, arguments);
+				}
+			}
+		}
+	}
+}
